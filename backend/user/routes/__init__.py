42e1d2619aa4679fe77f227890bd12ac7b3e74c2
ben23515225566 <ben23515225566@gmail.com>
2026-04-23 23:47:10 +0800

    Add routes

--- backend/user/routes/__init__.py
+++ backend/user/routes/__init__.py
@@ -1,4 +1,8 @@
 from routes.root import root_bp
+from routes.user import user_bp
+from routes.department import dept_bp
 
 def register_blueprints(app):
     app.register_blueprint(root_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(dept_bp)
---
+++ backend/user/routes/user.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify
+from controlers.user import user_controller
+
+
+user_bp = Blueprint('user', __name__, url_prefix='/user')
+
+@user_bp.route('/create', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = user_controller.createUser(data)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"message": "Failed to create user"}), 400
+
+@user_bp.route('/<int:id>', methods=['GET'])
+def get_user_by_id(id: int):
+    user = user_controller.getUserById(id)
+    if user:
+        return jsonify(user), 200
+    return jsonify({"message": "User not found"}), 404
