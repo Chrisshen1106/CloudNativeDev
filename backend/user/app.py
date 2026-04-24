7901882bd4179ee6e01639ade7e236b8b5398415
ben23515225566 <ben23515225566@gmail.com>
2026-04-24 22:20:17 +0800

    Update Database URI

--- backend/user/app.py
+++ backend/user/app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     register_blueprints(app)
     db.init_app(app)
