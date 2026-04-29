f9c238bc32ee19239b594d4e9fc94e77c9bbcf4e
ben23515225566 <ben23515225566@gmail.com>
2026-04-29 23:39:30 +0800

    Add JWT and password verification

--- backend/user/app.py
+++ backend/user/app.py
@@ -2,16 +2,20 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from routes import register_blueprints
+from flask_jwt_extended import JWTManager
 from models.database import db
 
 load_dotenv()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['JWT_SECRET_KEY'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     register_blueprints(app)
     db.init_app(app)
+    jwt.init_app(app)
     return app
 
 app = create_app()
--- backend/user/routes/user.py
+++ backend/user/routes/user.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, request, jsonify
-from marshmallow import ValidationError
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from utils.utils import verify_login
 from controllers.user import user_controller
 
 
-user_bp = Blueprint('user', __name__, url_prefix='/user')
+user_bp = Blueprint('user', __name__, url_prefix='/api')
 
-@user_bp.route('/create', methods=['POST'])
+@user_bp.route('/signup', methods=['POST'])
 def create_user():
     data = request.get_json()
+    data['password'] = pbkdf2_sha256.hash(data['password'])
     user = user_controller.createUser(data)
     if user:
         return jsonify(user), 200
@@ -28,10 +31,13 @@ def get_user_by_id(id: int):
 def login():
     try:
         data = request.get_json()
-        user = user_controller.getUserByEmail(data)
-        if user:
-            return jsonify(user), 200
-        return jsonify({"message": "User not found"}), 404
+        validated_data = user_controller.schema(only=['email', 'password']).load(data)
+        user = user_controller.getUserByEmail(validated_data['email'])
+        if user and verify_login(validated_data['password'], user.password):
+            response = user_controller.schema().dump(user)
+            response['token'] = create_access_token(identity=user.idUser)
+            return jsonify(response), 200
+        return jsonify({"message": "email or password is incorrect"}), 404
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
     except Exception as e:
