16697b0c3c961e5788d4f44edb53f6f563c8235e
王冠程 <r14725036@ntu.edu.tw>
2026-05-19 02:19:33 +0800

    feat: 新增測試 backend to S3 測試連線 function

--- backend/user/routes/user.py
+++ backend/user/routes/user.py
@@ -72,3 +72,16 @@ def get_all_users():
         return jsonify({"message": str(e)}), 400
     except Exception as e:
         return jsonify({"message": f"Internal server error: {str(e)}"}), 500
+
+# 測試在 eks 上能不能正常連線到 S3
+@user_bp.route('/users_test_s3', methods=['GET'])
+# @jwt_required()  # 1. 先註解掉驗證
+def get_all_users_test_s3():
+    try:
+        users = user_controller.getAllUsers()
+        response = user_controller.schema(many=True, only=['idUser', 'name', 'department']).dump(users)
+        return jsonify(response), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+    except Exception as e:
+        return jsonify({"message": f"Internal server error: {str(e)}"}), 500
